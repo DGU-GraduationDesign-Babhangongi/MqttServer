9433217eb6c1e18bffdf350cd6829c4be54a9119
smu207 <62317130+smu207@users.noreply.github.com>
2024-10-03 10:47:48 +0900

    test : 테스트 복원

--- src/main/java/donggukseoul/mqttServer/controller/ClassListController.java
+++ src/main/java/donggukseoul/mqttServer/controller/ClassListController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.stream.Stream;
 
 @RestController
-@RequestMapping("/api/classroomsss")
+@RequestMapping("/api/classrooms")
 @RequiredArgsConstructor
 public class ClassListController {
 
