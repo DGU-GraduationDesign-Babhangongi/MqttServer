8596015063523d0eec401011e1c5f4d80d250949
smu207 <62317130+smu207@users.noreply.github.com>
2024-10-03 10:00:26 +0900

    test : 젠킨스 테스트 복원

--- src/main/java/donggukseoul/mqttServer/controller/ClassListController.java
+++ src/main/java/donggukseoul/mqttServer/controller/ClassListController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.stream.Stream;
 
 @RestController
-@RequestMapping("/api/classroomstest")
+@RequestMapping("/api/classrooms")
 @RequiredArgsConstructor
 public class ClassListController {
 
