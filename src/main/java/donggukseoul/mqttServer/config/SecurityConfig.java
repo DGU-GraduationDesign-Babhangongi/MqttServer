951c22b58312f41a5dcecdcd997015fe24ecf1c1
smu207 <2019111988@dongguk.edu>
2024-10-11 17:44:33 +0900

    test : cors origin 테스트

--- src/main/java/donggukseoul/mqttServer/config/SecurityConfig.java
+++ src/main/java/donggukseoul/mqttServer/config/SecurityConfig.java
@@ -52,7 +52,7 @@ public class SecurityConfig {
                             public CorsConfiguration getCorsConfiguration(HttpServletRequest request) {
 
                                 CorsConfiguration configuration = new CorsConfiguration();
-                                configuration.setAllowedOrigins(Collections.singletonList("*"));
+                                configuration.setAllowedOrigins(Collections.singletonList("https://localhost:3000"));
                                 configuration.setAllowedMethods(Collections.singletonList("*"));
                                 configuration.setAllowCredentials(true);
                                 configuration.setAllowedHeaders(Collections.singletonList("*"));
