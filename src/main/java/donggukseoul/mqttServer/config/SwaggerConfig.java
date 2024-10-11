a0cc3cecc078540eb47162c6617476eb345b5d86
smu207 <2019111988@dongguk.edu>
2024-10-11 15:39:47 +0900

    update : origin 설정

---
+++ src/main/java/donggukseoul/mqttServer/config/SwaggerConfig.java
@@ -0,0 +1,24 @@
+//package donggukseoul.mqttServer.config;
+//
+//import io.swagger.v3.oas.models.OpenAPI;
+//import io.swagger.v3.oas.models.info.Info;
+//import lombok.RequiredArgsConstructor;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//
+//import java.util.Arrays;
+//
+//@Configuration
+//@RequiredArgsConstructor
+//public class SwaggerConfig {
+//    @Bean
+//    public OpenAPI openAPI() {
+//        Info info = new Info()
+//                .version("v1.0.0")
+//                .title("News Feed 프로젝트 테스트")
+//                .description("취준생을 위한 New Feed 프로젝트 swagger UI 테스트");
+//
+//        return new OpenAPI()
+//                .info(info);
+//    }
+//}
--- src/main/java/donggukseoul/mqttServer/config/WebConfig.java
+++ src/main/java/donggukseoul/mqttServer/config/WebConfig.java
@@ -13,11 +13,11 @@ public class WebConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOriginPatterns("*")
-//                        .allowedOrigins("*");  // 모든 도메인 허용
-                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")  // 허용할 HTTP 메서드
-                        .allowedHeaders("*")  // 모든 헤더 허용
-                        .allowCredentials(true);  // 자격 증명 허용
+//                        .allowedOriginPatterns("*")
+                        .allowedOrigins("*");  // 모든 도메인 허용
+//                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")  // 허용할 HTTP 메서드
+//                        .allowedHeaders("*")  // 모든 헤더 허용
+//                        .allowCredentials(true);  // 자격 증명 허용
             }
         };
     }
