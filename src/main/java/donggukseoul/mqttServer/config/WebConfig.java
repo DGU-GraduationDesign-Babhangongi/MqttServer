14a54d0d77b16d599226e29df0bf63ee57a202e9
smu207 <2019111988@dongguk.edu>
2024-10-11 17:32:24 +0900

    test : origin

--- src/main/java/donggukseoul/mqttServer/config/WebConfig.java
+++ src/main/java/donggukseoul/mqttServer/config/WebConfig.java
@@ -14,7 +14,7 @@ public class WebConfig {
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
 //                        .allowedOriginPatterns("*")
-                        .allowedOrigins("*","https://localhost:3000");  // 모든 도메인 허용
+                        .allowedOrigins("https://localhost:3000");  // 모든 도메인 허용
 //                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")  // 허용할 HTTP 메서드
 //                        .allowedHeaders("*")  // 모든 헤더 허용
 //                        .allowCredentials(true);  // 자격 증명 허용
